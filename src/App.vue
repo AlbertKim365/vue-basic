053178bc971bcdc0caa54e673e7e0e8fd095d6aa
AlbertKim365 <albert.kim365@gmail.com>
2025-01-15 17:47:08 +0900

    인라인, 메서드 핸들러

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,10 @@
 <template>
   <div>
-    <!-- <li>{{ sampleArray[0] }}</li>
-    <li>{{ sampleArray[1] }}</li>
-    <li>{{ sampleArray[2] }}</li>
-    <li>{{ sampleArray[3] }}</li> -->
+    <!-- <button v-on:click="count++">인라인 핸들러</button>
+    <h1>{{ count }}</h1> -->
 
-    <li
-      v-for="(item, index) in sampleArray"
-      :key="item"
-      @click="openDialog(index)"
-    >
-      {{ item }} / {{ index }}
-    </li>
-    <li v-for="(user, index) in otherArray" :key="user.name">
-      {{ user.id }} / {{ user.name }} / {{ index }}
-    </li>
-
-    <!-- <li>{{ sampleArray[0] }}</li>
-    <li>{{ sampleArray[1] }}</li>
-    <li>{{ sampleArray[2] }}</li>
-    <li>{{ sampleArray[3] }}</li> -->
+    <button v-on:click="changeName">메서드 핸들러</button>
+    <h1>{{ name }}</h1>
   </div>
 </template>
 
@@ -27,14 +12,14 @@
 export default {
   data() {
     return {
-      sampleArray: ["a", "b", "c", "d"],
-      otherArray: [
-        { id: 0, name: "John" },
-        { id: 1, name: "Kim" },
-        { id: 2, name: "Lee" },
-        { id: 3, name: "Park" },
-      ],
+      count: 0,
+      name: "Vue.js",
     };
+  },
+  methods: {
+    changeName() {
+      this.name = "변경된 텍스트 데이터 입니다.";
+    },
   },
 };
 </script>
