9a9b7a13e424fc017830492cb291db317382d699
AlbertKim365 <albert.kim365@gmail.com>
2025-01-15 11:13:04 +0900

    최초, 추가

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>{{ count }}</div>
+  <h1>Vue.js 라이프사이클 테스트</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  beforeCreate() {
+    console.log(`lifeCycle is beforeCreate`, this.count);
+  },
+  created() {
+    console.log(`olifeCycle is created`, this.count);
+    this.test();
+  },
+  // beforeMount() {
+  //   console.log(`lifeCycle is beforeMount`, document.querySelector("h1"));
+  // },
+  // Mounted() {
+  //   console.log(`olifeCycle is Mounted`, document.querySelector("h1"));
+  // },
+  methods: {
+    test() {
+      console.log(`함수 호출!!!`);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
