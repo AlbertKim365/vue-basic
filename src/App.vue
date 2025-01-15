f3dd3a2a8653c1ff68c981b6c7dd3de9b759e9fd
AlbertKim365 <albert.kim365@gmail.com>
2025-01-15 14:57:37 +0900

    스타일 바인딩딩

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,14 @@
   <h2 v-bind:class="{ active: isActive }">클래스 바인딩 테스트 입니다</h2>
   <h2 :class="{ active: isActive }">클래스 바인딩 테스트 입니다</h2>
   <button @click="change">버튼</button>
+
+  <h3 style="color: red; font-size: 24px">스타일 바인딩 테스트 입니다.</h3>
+  <h3 :style="{ color: '#888', fontSize: 48 + 'px' }">
+    스타일 바인딩 테스트 입니다.
+  </h3>
+  <h3 :style="{ color: fontColor, fontSize: fontSize + 'px' }">
+    스타일 바인딩 테스트 입니다2
+  </h3>
 </template>
 
 <script>
@@ -15,6 +23,8 @@ export default {
       rawHtml: "이것은 텍스트 입니다.",
       rawHtml2: "<span style='color: red'>이것은 빨간색 이어야 합니다.</span>",
       isActive: false,
+      fontColor: "#888888",
+      fontSize: "48",
     };
   },
   methods: {
